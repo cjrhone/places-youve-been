581bde608a713b7047cd3440f1619b1fee2d7d43
CJ Rhone and Dennise Ortega <dennise@>
2018-04-02 09:29:10 -0700

    Add functionality to displaying user input

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,38 @@
-// //business logic
-// function Place(location, landmarks, time,) {
-//   this.location=location;
-//   this.landmarks=landmarks;
-//   this.time=time;
-// }
-//
-// Place.prototype.allInfo = function() {
-//   return this.location + " " + this.time;
-// }
+//business logic
+function Place(location, landmarks, time,) {
+  this.location=location;
+  this.landmarks=landmarks;
+  this.time=time;
+}
+
+Place.prototype.allInfo = function() {
+  return this.location + " " + this.landmarks + " " + this.time;
+}
 
 
-//user interface logic
+// user interface logic
 
 $(document).ready(function() {
   $("form#trackLocation").submit(function(event) {
     event.preventDefault();
 
-    alert("hello!");
 
+
+
+  var inputtedLocation = $("input#newLocation").val();
+  var inputtedLandmarks = $("input#newLandmarks").val();
+  var inputtedTime = $("input#newTime").val();
+  var newPlace = new Place(inputtedLocation, inputtedLandmarks, inputtedTime);
+
+  $("ul#places").append("<li><span class='testPlace'>" + newPlace.allInfo() + "</span></li>")
+
+  $(".testPlace").last().click(function() {
   $("#show-location").show();
-
-  // var inputtedLocation = $("input#newLocation").val();
-  // var inputtedLandmarks = $("input#newLandmarks").val();
-  // var inputtedTime = $("input#newTime").val();
-  // var newPlace = new Place(inputtedLocation, inputtedLandmarks, inputtedTime);
-
+  $("#show-location h2").text(newPlace.location);
+  $(".location").text(newPlace.location);
+  $(".landmarks").text(newPlace.landmarks);
+  $(".time").text(newPlace.time);
+});
 
 
 });
